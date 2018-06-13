7b860045ce2d78929025e580a1813ee00d834f4b
Cezary Witkowski <czarecoo@gmail.com>
2018-06-13 15:45:48 +0200

    dodane testy invoice generatora

---
+++ src/zpi/taxcalculator/test/model/InvoiceGeneratorTest.java
@@ -0,0 +1,38 @@
+package model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class InvoiceGeneratorTest {
+
+    Product product1 = new Product(Product.ProductType.GROCERIES,25f);
+    Product product2 = new Product(Product.ProductType.PREPARED_FOOD, 10f);
+   // TaxPolicy policy1 = new TaxPolicy(new TaxData("Alabama",4,"b","b","b","b","b","b"));
+    TaxPolicy policy1 = new TaxPolicy(new TaxData("Alabama", 4, "b", "b", "e", "b", "b", "b"));
+    TaxPolicy policy2 = new TaxPolicy(new TaxData("Massachusetts", 6, "e", "b", "e", "b", "e,>175", "e"));
+
+    List<Product> productList = new ArrayList<Product>();
+    List<TaxPolicy> policyList = new ArrayList<TaxPolicy>();
+
+    List<InvoiceEntry> invoice = new ArrayList<>();
+
+    @Before
+    public void setUp() throws Exception {
+        productList.add(product1);
+        productList.add(product2);
+        policyList.add(policy1);
+        policyList.add(policy2);
+
+    }
+
+    @Test
+    public void generateInvoice() {
+        invoice = InvoiceGenerator.generateInvoice(productList,policyList);
+
+    }
+}
