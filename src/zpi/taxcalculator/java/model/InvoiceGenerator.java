626c5ff34d446a2885b9bb5503828eb4a24629ba
Marcin Kapelan <marcinkape@gmail.com>
2018-06-13 19:04:44 +0200

    Fix grossPrice calculation

--- src/zpi/taxcalculator/java/model/InvoiceGenerator.java
+++ src/zpi/taxcalculator/java/model/InvoiceGenerator.java
@@ -14,7 +14,7 @@ public class InvoiceGenerator {
         for (int i = 0; i < productList.size() ; i++) {
             Product product = productList.get(i);
             Tax tax = taxPolicyList.get(i).calculateTax(product);
-            Float grossPrice = product.getNetPrice() * tax.getTaxValuePercent();
+            Float grossPrice = product.getNetPrice() * (1 + tax.getTaxValuePercent() / 100);
             invoiceEntryList.add(new InvoiceEntry(productList.get(i), tax, grossPrice));
         }
         return invoiceEntryList;
