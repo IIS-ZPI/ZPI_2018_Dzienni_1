4e25cd7fae8d3ab7756a62ec814bd74af22cb4e7
Piotr Sadza <5673@gmail.com>
2018-06-13 23:06:16 +0200

    Added controller implementation and changed look of display window.

--- src/zpi/taxcalculator/java/view/RootLayoutController.java
+++ src/zpi/taxcalculator/java/view/RootLayoutController.java
@@ -1,4 +1,56 @@
 package view;
 
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import model.TaxData;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class RootLayoutController {
+
+    private Map<String, TaxData> data;
+    private List<String> listOfStates;
+
+
+    @FXML
+    private ComboBox<String> comboBox;
+
+    @FXML
+    private TextField sellingPriceTextField;
+
+    @FXML
+    private TextField buyingPriceTextField;
+
+    @FXML
+    private TableView table;
+    @FXML
+    private Button buttonCalculate;
+
+    @FXML
+    public void initialize() {
+//        Map<String, TaxData> data = new HashMap<>();
+//        DataLoader dataLoader = new DataLoader( "data/data.tsv");
+//        data = dataLoader.getData();
+//        listOfStates = data.keySet().stream().collect(Collectors.toList());
+//        comboBox.setItems(FXCollections.observableList(listOfStates));
+//        comboBox.getSelectionModel().selectFirst();
+    }
+
+    @FXML
+    public void calculate() {
+        //Use regex in future for security
+        Float sellingPrice = Float.valueOf(sellingPriceTextField.getCharacters().toString());
+        Float buyingPrice = Float.valueOf(buyingPriceTextField.getCharacters().toString());
+        System.out.println("Not implemented yet !");
+        //TO-DO
+    }
+
+
+
 }
